949edd023a2673b054b4572d970129c506ef4b42
Mesut MUTLU <mesut_mutlu83@yahoo.fr>
2019-04-04 15:01:19 +0300

    -

--- earthquake/read_file.py
+++ earthquake/read_file.py
@@ -6,14 +6,18 @@ import psutil
 import os
 from datetime import datetime
 base_path = r"C:\datasets\earthquake"
-import multiprocessing
+from multiprocessing import Pool, Manager
+import time
 train_path = os.path.join(base_path, "train/train.csv")
-test_file_path = os.path.join(base_path, "test")
+test_file_path = os.path.join(base_path, "test/")
+df_list = []
+rows = 10_000
+w_sizes = [10, 100, 1000]
 
-
-def seg_rolling_features(seg, seg_id , w_sizes):
-    print("start", seg_id, datetime.now())
+def seg_rolling_features(seg_id, seg, w_sizes):
+    # print("start", seg_id, datetime.now())
     X_df = pd.DataFrame(index=[seg_id], dtype=np.float64)
+
     X_df.loc[seg_id, 'mean'] = seg.mean()
     X_df.loc[seg_id, 'std'] = seg.std()
     X_df.loc[seg_id, 'max'] = seg.max()
@@ -27,7 +31,17 @@ def seg_rolling_features(seg, seg_id , w_sizes):
     X_df.loc[seg_id, "quantile_0.8_mean"] = seg.quantile(0.8, interpolation="linear")
 
     for window in w_sizes:
+        seg_roll_std = seg.rolling(window).std()
+        seg_roll_mean = seg.rolling(window).mean()
+        seg_roll_var = seg.rolling(window).var()
         seg_roll_min = seg.rolling(window).min()
+        seg_roll_max = seg.rolling(window).max()
+        seg_roll_sum = seg.rolling(window).sum()
+
+        seg_emw_std = seg.ewm(span=window).std()
+        seg_emw_mean = seg.ewm(span=window).mean()
+        seg_emw_var = seg.ewm(span=window).var()
+
         X_df.loc[seg_id, "rolling_min_min_" + str(window)] = seg_roll_min.min()
         X_df.loc[seg_id, "rolling_min_sum_" + str(window)] = seg_roll_min.sum()
         X_df.loc[seg_id, "rolling_min_maseg_" + str(window)] = seg_roll_min.max()
@@ -38,8 +52,190 @@ def seg_rolling_features(seg, seg_id , w_sizes):
         X_df.loc[seg_id, "rolling_min_quan04_" + str(window)] = seg_roll_min.quantile(0.4)
         X_df.loc[seg_id, "rolling_min_quan06_" + str(window)] = seg_roll_min.quantile(0.6)
         X_df.loc[seg_id, "rolling_min_quan08_" + str(window)] = seg_roll_min.quantile(0.8)
-    print("end", seg_id, datetime.now())
+
+        X_df.loc[seg_id, "rolling_maseg_min_" + str(window)] = seg_roll_max.min()
+        X_df.loc[seg_id, "rolling_maseg_sum_" + str(window)] = seg_roll_max.sum()
+        X_df.loc[seg_id, "rolling_maseg_maseg_" + str(window)] = seg_roll_max.max()
+        X_df.loc[seg_id, "rolling_maseg_std_" + str(window)] = seg_roll_max.std()
+        X_df.loc[seg_id, "rolling_maseg_var_" + str(window)] = seg_roll_max.var()
+        X_df.loc[seg_id, "rolling_maseg_mean_" + str(window)] = seg_roll_max.mean()
+        X_df.loc[seg_id, "rolling_maseg_quan02_" + str(window)] = seg_roll_max.quantile(0.2)
+        X_df.loc[seg_id, "rolling_maseg_quan04_" + str(window)] = seg_roll_max.quantile(0.4)
+        X_df.loc[seg_id, "rolling_maseg_quan06_" + str(window)] = seg_roll_max.quantile(0.6)
+        X_df.loc[seg_id, "rolling_maseg_quan08_" + str(window)] = seg_roll_max.quantile(0.8)
+
+        X_df.loc[seg_id, "rolling_std_min_" + str(window)] = seg_roll_std.min()
+        X_df.loc[seg_id, "rolling_std_sum_" + str(window)] = seg_roll_std.sum()
+        X_df.loc[seg_id, "rolling_std_maseg_" + str(window)] = seg_roll_std.max()
+        X_df.loc[seg_id, "rolling_std_std_" + str(window)] = seg_roll_std.std()
+        X_df.loc[seg_id, "rolling_std_var_" + str(window)] = seg_roll_std.var()
+        X_df.loc[seg_id, "rolling_std_mean_" + str(window)] = seg_roll_std.mean()
+        X_df.loc[seg_id, "rolling_std_quan02_" + str(window)] = seg_roll_std.quantile(0.2)
+        X_df.loc[seg_id, "rolling_std_quan04_" + str(window)] = seg_roll_std.quantile(0.4)
+        X_df.loc[seg_id, "rolling_std_quan06_" + str(window)] = seg_roll_std.quantile(0.6)
+        X_df.loc[seg_id, "rolling_std_quan08_" + str(window)] = seg_roll_std.quantile(0.8)
+
+        X_df.loc[seg_id, "rolling_sum_min_" + str(window)] = seg_roll_sum.min()
+        X_df.loc[seg_id, "rolling_sum_sum_" + str(window)] = seg_roll_sum.sum()
+        X_df.loc[seg_id, "rolling_sum_maseg_" + str(window)] = seg_roll_sum.max()
+        X_df.loc[seg_id, "rolling_sum_sum_" + str(window)] = seg_roll_sum.std()
+        X_df.loc[seg_id, "rolling_sum_var_" + str(window)] = seg_roll_sum.var()
+        X_df.loc[seg_id, "rolling_sum_mean_" + str(window)] = seg_roll_sum.mean()
+        X_df.loc[seg_id, "rolling_sum_quan02_" + str(window)] = seg_roll_sum.quantile(0.2)
+        X_df.loc[seg_id, "rolling_sum_quan04_" + str(window)] = seg_roll_sum.quantile(0.4)
+        X_df.loc[seg_id, "rolling_sum_quan06_" + str(window)] = seg_roll_sum.quantile(0.6)
+        X_df.loc[seg_id, "rolling_sum_quan08_" + str(window)] = seg_roll_sum.quantile(0.8)
+
+        X_df.loc[seg_id, "rolling_var_min_" + str(window)] = seg_roll_var.min()
+        X_df.loc[seg_id, "rolling_var_sum_" + str(window)] = seg_roll_var.sum()
+        X_df.loc[seg_id, "rolling_var_maseg_" + str(window)] = seg_roll_var.max()
+        X_df.loc[seg_id, "rolling_var_std_" + str(window)] = seg_roll_var.std()
+        X_df.loc[seg_id, "rolling_var_var_" + str(window)] = seg_roll_var.var()
+        X_df.loc[seg_id, "rolling_var_mean_" + str(window)] = seg_roll_var.mean()
+        X_df.loc[seg_id, "rolling_var_quan02_" + str(window)] = seg_roll_var.quantile(0.2)
+        X_df.loc[seg_id, "rolling_var_quan04_" + str(window)] = seg_roll_var.quantile(0.4)
+        X_df.loc[seg_id, "rolling_var_quan06_" + str(window)] = seg_roll_var.quantile(0.6)
+        X_df.loc[seg_id, "rolling_var_quan08_" + str(window)] = seg_roll_var.quantile(0.8)
+
+        X_df.loc[seg_id, "rolling_mean_min_" + str(window)] = seg_roll_mean.min()
+        X_df.loc[seg_id, "rolling_mean_sum_" + str(window)] = seg_roll_mean.sum()
+        X_df.loc[seg_id, "rolling_mean_maseg_" + str(window)] = seg_roll_mean.max()
+        X_df.loc[seg_id, "rolling_mean_std_" + str(window)] = seg_roll_mean.std()
+        X_df.loc[seg_id, "rolling_mean_var_" + str(window)] = seg_roll_mean.var()
+        X_df.loc[seg_id, "rolling_mean_mean_" + str(window)] = seg_roll_mean.mean()
+        X_df.loc[seg_id, "rolling_mean_quan02_" + str(window)] = seg_roll_mean.quantile(0.2)
+        X_df.loc[seg_id, "rolling_mean_quan04_" + str(window)] = seg_roll_mean.quantile(0.4)
+        X_df.loc[seg_id, "rolling_mean_quan06_" + str(window)] = seg_roll_mean.quantile(0.6)
+        X_df.loc[seg_id, "rolling_mean_quan08_" + str(window)] = seg_roll_mean.quantile(0.8)
+
+        X_df.loc[seg_id, "ewm_mean_min_" + str(window)] = seg_emw_mean.min()
+        X_df.loc[seg_id, "ewm_mean_sum_" + str(window)] = seg_emw_mean.sum()
+        X_df.loc[seg_id, "ewm_mean_maseg_" + str(window)] = seg_emw_mean.max()
+        X_df.loc[seg_id, "ewm_mean_std_" + str(window)] = seg_emw_mean.std()
+        X_df.loc[seg_id, "ewm_mean_var_" + str(window)] = seg_emw_mean.var()
+        X_df.loc[seg_id, "ewm_mean_mean_" + str(window)] = seg_emw_mean.mean()
+        X_df.loc[seg_id, "ewm_mean_quan02_" + str(window)] = seg_emw_mean.quantile(0.2)
+        X_df.loc[seg_id, "ewm_mean_quan04_" + str(window)] = seg_emw_mean.quantile(0.4)
+        X_df.loc[seg_id, "ewm_mean_quan06_" + str(window)] = seg_emw_mean.quantile(0.6)
+        X_df.loc[seg_id, "ewm_mean_quan08_" + str(window)] = seg_emw_mean.quantile(0.8)
+
+        X_df.loc[seg_id, "ewm_std_min_" + str(window)] = seg_emw_std.min()
+        X_df.loc[seg_id, "ewm_std_sum_" + str(window)] = seg_emw_std.sum()
+        X_df.loc[seg_id, "ewm_std_maseg_" + str(window)] = seg_emw_std.max()
+        X_df.loc[seg_id, "ewm_std_std_" + str(window)] = seg_emw_std.std()
+        X_df.loc[seg_id, "ewm_std_var_" + str(window)] = seg_emw_std.var()
+        X_df.loc[seg_id, "ewm_std_mean_" + str(window)] = seg_emw_std.mean()
+        X_df.loc[seg_id, "ewm_std_quan02_" + str(window)] = seg_emw_std.quantile(0.2)
+        X_df.loc[seg_id, "ewm_std_quan04_" + str(window)] = seg_emw_std.quantile(0.4)
+        X_df.loc[seg_id, "ewm_std_quan06_" + str(window)] = seg_emw_std.quantile(0.6)
+        X_df.loc[seg_id, "ewm_std_quan08_" + str(window)] = seg_emw_std.quantile(0.8)
+
+        X_df.loc[seg_id, "ewm_var_min_" + str(window)] = seg_emw_var.min()
+        X_df.loc[seg_id, "ewm_var_sum_" + str(window)] = seg_emw_var.sum()
+        X_df.loc[seg_id, "ewm_var_maseg_" + str(window)] = seg_emw_var.max()
+        X_df.loc[seg_id, "ewm_var_std_" + str(window)] = seg_emw_var.std()
+        X_df.loc[seg_id, "ewm_var_var_" + str(window)] = seg_emw_var.var()
+        X_df.loc[seg_id, "ewm_var_mean_" + str(window)] = seg_emw_var.mean()
+        X_df.loc[seg_id, "ewm_var_quan02_" + str(window)] = seg_emw_var.quantile(0.2)
+        X_df.loc[seg_id, "ewm_var_quan04_" + str(window)] = seg_emw_var.quantile(0.4)
+        X_df.loc[seg_id, "ewm_var_quan06_" + str(window)] = seg_emw_var.quantile(0.6)
+        X_df.loc[seg_id, "ewm_var_quan08_" + str(window)] = seg_emw_var.quantile(0.8)
     return X_df
+
+def collect_result(result):
+    df_list.append(result)
+
+def calc_train_features(train, df_list):
+    print("Start date", datetime.now())
+    segments = int(np.floor(train.shape[0] / rows))
+
+    y_train = pd.DataFrame(index=range(segments), dtype=np.float64, columns=['time_to_failure'])
+    # X_train = pd.DataFrame(index=range(segments), dtype=np.float64)
+    X_train = None
+
+    p = Pool(4)
+
+    # for seg_id in tqdm_notebook(range(segments)):
+    i = 1
+    for seg_id in range(segments):
+        if i< 6:
+            seg = train.iloc[seg_id * rows:seg_id * rows + rows]
+
+            y_train.loc[seg_id, 'time_to_failure'] = seg['time_to_failure'].values[-1]
+
+            # x_seg = pd.Series(seg['acoustic_data'].values)
+            # X_train = seg_rolling_features(seg_id, seg['acoustic_data'], [10, 100, 1000], X_train)
+            p.apply_async(seg_rolling_features, [seg_id, seg['acoustic_data'], w_sizes], callback=collect_result)
+            i += 1
+        else:
+            break
+    p.close()
+    p.join()
+
+    for df in df_list:
+        if X_train is None:
+            X_train = df
+        else:
+            X_train = pd.concat([X_train, df])
+
+    print("End date", datetime.now())
+
+    print("X_train shape:", X_train.shape)
+    print(X_train.head())
+    X_train.to_csv("./output/xtrain.csv", index=False)
+    print("y_train shape:", y_train.shape)
+    print(y_train.head())
+    y_train.to_csv("./output/ytrain.csv", index=False)
+
+    return X_train, y_train
+
+def calc_test_features(df_list):
+    print("Start date", datetime.now())
+
+    submission = pd.read_csv(os.path.join(base_path, 'sample_submission.csv'), index_col='seg_id')
+    # X_test = pd.DataFrame(columns=X_train.columns, dtype=np.float64, index=submission.index)
+    X_test = None
+    p = Pool(2)
+
+    m = Manager()
+    q = m.Queue()
+    # for i, seg_id in enumerate(tqdm_notebook(X_test.index)):
+    i = 0
+    for i, seg_id in enumerate(submission.index):
+
+        if i < 6:
+            seg = pd.read_csv(test_file_path + seg_id + '.csv')['acoustic_data'].values
+            x_seg = pd.Series(seg)
+            # X_test = seg_rolling_features(seg_id, x_seg, [10, 100, 1000], X_test)
+            res = p.apply_async(seg_rolling_features, [seg_id, x_seg, w_sizes], callback=collect_result)
+            print('I am on number ' + str(i + 1) + ' of ' + str(len(submission.index)))
+            i += 1
+        else:
+            break
+    if 1 == 1:
+        while True:
+            if res.ready():
+                break
+            else:
+                size = q.qsize()
+                print(size)
+                time.sleep(2)
+
+    p.close()
+    p.join()
+    print(df_list)
+    for df in df_list:
+        if X_test is None:
+            X_test = df
+        else:
+            X_test = pd.concat([X_test, df])
+
+    print("End date", datetime.now())
+
+    print("X_test shape:", X_test.shape)
+    print(X_test.head())
+    X_test.to_csv("./output/xtest.csv", index=False)
+    return X_test
 
 
 
@@ -49,34 +245,4 @@ if __name__ == "__main__":
 
     train.columns = ["acoustic_data", "time_to_failure"]
 
-    rows = 100_000
-    w_sizes = [10, 100, 1000]
-
-    segments = int(np.floor(train.shape[0] / rows))
-    print(segments)
-    df_list= []
-
-
-    def collect_result(result):
-        df_list.append(result)
-
-
-    p = multiprocessing.Pool(2)
-    for seg_id in (range(segments)):
-        if seg_id < 15:
-            print(seg_id)
-            seg = train.iloc[seg_id * rows:seg_id * rows + rows]
-
-            p.apply_async(seg_rolling_features, [seg['acoustic_data'], seg_id, w_sizes], callback=collect_result)
-    p.close()
-    p.join()
-    X_train = None
-    #print(df_list)
-    for x in df_list:
-        #print(type(x))
-        if X_train is None:
-            X_train = x
-        else:
-            X_train = pd.concat([X_train,x])
-    print(X_train)
-    sys.exit()
+    calc_test_features(df_list)
--- earthquake/train.py
+++ earthquake/train.py
@@ -5,92 +5,131 @@ import numpy as np
 from datetime import datetime
 import time
 from matplotlib import pyplot as plt
+import random
 
-X_train = pd.read_csv(r"C:\datasets\earthquake\xtrain.csv")
-X_train.drop("Unnamed: 0", inplace=True, axis=1)
-print(X_train.head())
-y_train = pd.read_csv(r"C:\datasets\earthquake\ytrain.csv")
-y_train.drop("Unnamed: 0", inplace=True, axis=1)
-print(y_train.head())
+def fit_eval(X_train, y_train, epoch, batch_size, optimizer, activation, dropout):
+    kfold = RepeatedKFold(n_splits=5, n_repeats=2, random_state=2652124)
+    cv_train_scores = []
+    cv_train_loss = []
+    cv_val_scores = []
+    cv_val_loss = []
 
-for c in (X_train.columns.values.tolist()):
-    mean = X_train[c].mean()
-    X_train[c] = X_train[c] - mean
-    #X_test[c] = X_test[c] - mean
-    std = X_train[c].std()
-    X_train[c] = X_train[c] / std
-    #X_test[c] = X_test[c] / std
+    for train, val in kfold.split(X_train, y_train):
+        # create model
+        print(datetime.now(), len(train), len(val))
+        model = models.Sequential()
+        model.add(layers.Dense(150, input_dim=(X_train.shape[1]), activation='relu',
+                               kernel_regularizer=regularizers.l2(0.001)))
+        model.add(layers.Dropout(dropout))
+        model.add(layers.Dense(150, activation='relu',
+                               kernel_regularizer=regularizers.l2(0.001)))
+        model.add(layers.Dropout(dropout))
+        if activation == "none":
+            model.add(layers.Dense(1, activation="relu"))
+        else:
+            model.add(layers.Dense(1, activation=activation))
+        # Compile model
+        model.compile(loss='mse', optimizer=optimizer, metrics=['mae'])
+        # Fit the model
+        history = model.fit(X_train.iloc[train].values, y_train.iloc[train].values.ravel(), epochs=epoch, verbose=0,
+                            batch_size=batch_size,
+                            validation_data=(X_train.iloc[val].values, y_train.iloc[val].values.ravel()))
+        history_dict = history.history
+        # collect score and loss values
+        # print(history_dict)
+        cv_train_loss.append(history_dict['loss'])
+        cv_val_loss.append(history_dict['val_loss'])
+        cv_train_scores.append(history_dict['mean_absolute_error'])
+        cv_val_scores.append(history_dict['val_mean_absolute_error'])
 
-pd.concat([X_train, y_train], axis=1).corr()[["time_to_failure"]].to_csv("./output/pearson_corr.csv")
+    if 1 == 0:
+        pred = model.predict(X_train)
+        print(pred.shape)
+        pred = np.abs(pred)
+        print(y_train["time_to_failure"].values.reshape(-1, 1))
+        pred_df = pd.DataFrame(data=np.concatenate((y_train["time_to_failure"].values.reshape(-1, 1), pred), axis=1),
+                               columns=["gt", "pr"])
+
+    if 1 == 0:
+        print(pred_df.to_csv("./output/pred.csv"))
+        # sum score and loss values for different CVs
+        train_loss = [sum(x) for x in zip(*cv_train_loss)][80:]
+        val_loss = [sum(x) for x in zip(*cv_val_loss)][80:]
+        train_scores = [sum(x) for x in zip(*cv_train_scores)][80:]
+        val_scores = [sum(x) for x in zip(*cv_val_scores)][80:]
+
+        plt.figure(figsize=(25, 10))
+        plt.plot(range(1, len(train_loss) + 1), train_loss, 'bo', label='Training loss')
+        plt.plot(range(1, len(val_loss) + 1), val_loss, 'b', label='Validation loss')
+        plt.title("Trainin and validation loss")
+        plt.xlabel('Epochs')
+        plt.ylabel('Loss')
+        plt.legend()
+
+        plt.show()
+
+        plt.figure(figsize=(25, 10))
+        plt.plot(range(1, len(train_scores) + 1), train_scores, 'bo', label='Training mae')
+        plt.plot(range(1, len(val_scores) + 1), val_scores, 'b', label='Validation mae')
+        plt.title("Trainin and validation mae")
+        plt.xlabel('Epochs')
+        plt.ylabel('Mae')
+        plt.legend()
+
+        plt.show()
+
+    return epoch, batch_size, optimizer, activation, model.evaluate(X_train, y_train.values.ravel())
+
+if __name__ == "__main__":
+    X_train = pd.read_csv(r"C:\datasets\earthquake\xtrain.csv")
+    X_train.drop("Unnamed: 0", inplace=True, axis=1)
+    print(X_train.head())
+    y_train = pd.read_csv(r"C:\datasets\earthquake\ytrain.csv")
+    y_train.drop("Unnamed: 0", inplace=True, axis=1)
+    print(y_train.head())
+
+    X_test = pd.read_csv(r"C:\datasets\earthquake\xtest.csv")
+    #X_test.drop("Unnamed: 0", inplace=True, axis=1)
+    print(X_test.head())
+
+    for c in (X_train.columns.values.tolist()):
+        mean = X_train[c].mean()
+        X_train[c] = X_train[c] - mean
+        X_test[c] = X_test[c] - mean
+        std = X_train[c].std()
+        X_train[c] = X_train[c] / std
+        X_test[c] = X_test[c] / std
+
+    pd.concat([X_train, y_train], axis=1).corr()[["time_to_failure"]].to_csv("./output/pearson_corr.csv")
 
 
-kfold = RepeatedKFold(n_splits=5, n_repeats=2, random_state=2652124)
-cv_train_scores = []
-cv_train_loss = []
-cv_val_scores = []
-cv_val_loss = []
+    epochs = [250, 500, 1000, 1500, 2000, 2500]
+    batch_sizes = [64, 128, 256]
+    optimizers = ["adam", "rmsprop"]
+    activations= ["relu", "none"]
+    dropouts = [0.1, 0.2, 0.3, 0.4]
+    n_grid = 10
+    scores = pd.DataFrame(index=[i for i in range(n_grid)],
+                          columns=["epoch", "batch_size", "optimizer", "activation", "dropout", "score", "start_date", "end_date"])
+    for i in range(n_grid):
+        r_epoch = random.choice(epochs)
+        r_batch_size = random.choice(batch_sizes)
+        r_optimizer = random.choice(optimizers)
+        r_activation = random.choice(activations)
+        r_dropout = random.choice(dropouts)
+        start_date = datetime.now()
+        print(i, datetime.now(),r_epoch, r_batch_size,  r_optimizer, r_activation, r_dropout)
+        epoch, batch_size, optimizer, activation, score = \
+            fit_eval(X_train, y_train, r_epoch, r_batch_size, r_optimizer, r_activation, r_dropout)
+        end_date = datetime.now()
+        scores.loc[i, "epoch"] = epoch
+        scores.loc[i, "batch_size"] = batch_size
+        scores.loc[i, "optimizer"] = optimizer
+        scores.loc[i, "activation"] = activation
+        scores.loc[i, "score"] = score
+        scores.loc[i, "start_date"] = start_date
+        scores.loc[i, "end_date"] = end_date
 
-cv_history = pd.DataFrame(dtype=np.float64, index=range(1, 11))
+    scores.sort_values(by=["score"]).to_csv("./output/scores.csv", index=False)
 
-for train, val in kfold.split(X_train, y_train):
-    # create model
-    print(datetime.now(), len(train), len(val))
-    model = models.Sequential()
-    model.add(layers.Dense(150, input_dim=(X_train.shape[1]), activation='relu',
-                           kernel_regularizer=regularizers.l2(0.001)))
-    model.add(layers.Dropout(0.1))
-    model.add(layers.Dense(150, activation='relu',
-                           kernel_regularizer=regularizers.l2(0.001)))
-    model.add(layers.Dropout(0.1))
-    model.add(layers.Dense(1, activation="relu"))
-    # Compile model
-    model.compile(loss='mse', optimizer='adam', metrics=['mae'])
-    # Fit the model
-    history = model.fit(X_train.iloc[train].values, y_train.iloc[train].values.ravel(), epochs=500, verbose=0,
-                        batch_size=64, validation_data=(X_train.iloc[val].values, y_train.iloc[val].values.ravel()))
-    history_dict = history.history
-    # collect score and loss values
-    # print(history_dict)
-    cv_train_loss.append(history_dict['loss'])
-    cv_val_loss.append(history_dict['val_loss'])
-    cv_train_scores.append(history_dict['mean_absolute_error'])
-    cv_val_scores.append(history_dict['val_mean_absolute_error'])
-
-pred = model.predict(X_train)
-print(pred.shape)
-pred = np.abs(pred)
-print(y_train["time_to_failure"].values.reshape(-1,1))
-pred_df = pd.DataFrame(data=np.concatenate((y_train["time_to_failure"].values.reshape(-1,1),pred), axis=1), columns=["gt", "pr"])
-
-
-
-print(pred_df.to_csv("./output/pred.csv"))
-# sum score and loss values for different CVs
-train_loss = [sum(x) for x in zip(*cv_train_loss)][80:]
-val_loss = [sum(x) for x in zip(*cv_val_loss)][80:]
-train_scores = [sum(x) for x in zip(*cv_train_scores)][80:]
-val_scores = [sum(x) for x in zip(*cv_val_scores)][80:]
-
-plt.figure(figsize=(25, 10))
-plt.plot(range(1, len(train_loss) + 1), train_loss, 'bo', label='Training loss')
-plt.plot(range(1, len(val_loss) + 1), val_loss, 'b', label='Validation loss')
-plt.title("Trainin and validation loss")
-plt.xlabel('Epochs')
-plt.ylabel('Loss')
-plt.legend()
-
-plt.show()
-
-plt.figure(figsize=(25, 10))
-plt.plot(range(1, len(train_scores) + 1), train_scores, 'bo', label='Training mae')
-plt.plot(range(1, len(val_scores) + 1), val_scores, 'b', label='Validation mae')
-plt.title("Trainin and validation mae")
-plt.xlabel('Epochs')
-plt.ylabel('Mae')
-plt.legend()
-
-plt.show()
-
-
-print(model.evaluate(X_train, y_train.values.ravel()))
 
