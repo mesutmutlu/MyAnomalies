734d30840e98bf24bd97b71084caecce3cbac996
Mesut MUTLU <mesut_mutlu83@yahoo.fr>
2019-04-25 10:17:50 +0300

    -

--- reco_engine/model_creators/content_memory_based_model.py
+++ reco_engine/model_creators/content_memory_based_model.py
@@ -4,11 +4,13 @@ import sys
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.metrics.pairwise import linear_kernel, cosine_similarity
 from reco_engine.prepare_data import prepare_movies_data
+from ast import literal_eval
 
-def text_based_cos_sim(df):
+text_cos_sims = ["overview", "cast" , "keyword", "leads", "genres", "belongs_to_collection"]
+def text_based_cos_sim(df, n):
     # Define a TF-IDF Vectorizer Object. Remove all english stopwords
     tfidf = TfidfVectorizer(stop_words='english')
-    n=20000
+
     # Replace NaN with an empty string
     df.fillna('', inplace=True)
     tfidf_matrix = tfidf.fit_transform(df)
@@ -16,20 +18,20 @@ def text_based_cos_sim(df):
     print(tfidf_matrix.shape, cosine_sim.shape)
     return cosine_sim
 
-def crete_text_based_cos_sim_models():
+def cre_text_cos_sim_model(movies_df, feature):
+    n = 10000
     movies_df = pd.read_csv("C:/datasets/the-movies-dataset/prep_data.csv")
-
-    movies_df_overview_sim = text_based_cos_sim(movies_df["overview"])
-    movies_df["str_cast"] =  movies_df["cast"].apply(' '.join)
-    print(movies_df["str_cast"])
-    movies_df_overview_sim = text_based_cos_sim(movies_df["overview"].apply())
-
-
+    cos_sim = text_based_cos_sim(movies_df[feature], n)
+    print(cos_sim.shape)
+    pd.DataFrame(data=cos_sim, index = movies_df.id[:n], columns=movies_df.id[:n]).to_csv("movies_df_"+feature+"_cos_sim.csv")
 
 if __name__ == "__main__":
+    sys.stdout.buffer.write(chr(9986).encode('utf8'))
+    pd.set_option('display.max_columns', 500)
+    pd.set_option('display.width', 1000)
     #movies_df = prepare_movies_data()
     movies_df = pd.read_csv("C:/datasets/the-movies-dataset/prep_data.csv")
     print(movies_df.head())
-    movies_df["str_cast"] = movies_df["cast"].apply(lambda x: ' '.join(x))
-    print(movies_df["str_cast"])
-    #print(text_based_cos_similarity(movies_df["overview"]))
+
+    for f in text_cos_sims:
+        cre_text_cos_sim_model(movies_df, f)
