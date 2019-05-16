d53cd8fb104f599d278d2857bbd2f0c6dc87195d
Mesut MUTLU <mesut_mutlu83@yahoo.fr>
2019-05-16 15:03:40 +0300

    -

--- reco_engine/recommenders/coll_filt_based.py
+++ reco_engine/recommenders/coll_filt_based.py
@@ -105,53 +105,55 @@ class CosSim_Recommender(Base_Recommender):
         #print(predicted_ratings)
         return predicted_ratings.sort_values(by=["pre_rating"], ascending=False)[:n].reset_index()
 
-class SVD_Recommender(Base_Recommender):
+class SVDPP_Recommender(Base_Recommender):
     def __init__(self, type):
         Base_Recommender.__init__(self, type)
 
     def create_model(self):
-        n = 1000
-
-        raw_data = self.get_data()[:n].fillna(0)[["userId", "id", "rating"]]
+        n = 1000000
+        raw_data = self.get_ratings()[:n].fillna(0)[["userId", "id", "rating"]]
         reader = Reader()
         data = Dataset.load_from_df(raw_data, reader)
         data.split(n_folds=5)
         svdpp = SVDpp()
         trainset = data.build_full_trainset()
         svdpp.fit(trainset)
-        l_usr = User_Helper.get_user_list()
-        l_cnt = Content_Helper.get_content_list()
         filename = "C:/datasets/the-movies-dataset/models/collaborative_based/coll_svdpp.sav"
-        #col_sid = ["sid_" + str(i) for i in range(1, n + 1)]
-        #col_sim = ["sim_" + str(i) for i in range(1, n + 1)]
-        # predicted_ratings = pd.DataFrame(index=l_usr["userid"].values.tolist(), columns=l_cnt.index.tolist())
-        # #print(predicted_ratings)
-        # #print(l_cnt)
-        # i = 0
-        # for c in l_cnt.index.tolist():
-        #     #print(c.id)
-        #     for u in l_usr["userid"].values.tolist():
-        #         print(i, len(l_cnt)* len(l_usr))
-        #         predicted_ratings.loc[u, c] = svdpp.predict(u, c).est
-        #         #predicted_ratings.loc[u["userId"],id] = svdpp.predict(u["userId"], c["id"]).est
-        # #movies['est'] = raw_data['id'].apply(lambda x: svd.predict(userId, id_to_title.loc[x]['movieId']).est)
-        #         i += 1
-        # print(predicted_ratings)
         joblib.dump(svdpp, filename)
 
-    def make_recommendation_by_user_movie(self, userid, id):
+    def predict_rating_by_user_movie(self, userid, id):
         svdpp = joblib.load("C:/datasets/the-movies-dataset/models/collaborative_based/coll_svdpp.sav")
         return svdpp.predict(userid, id).est
+
+class SVD_Recommender(Base_Recommender):
+    def __init__(self, type):
+        Base_Recommender.__init__(self, type)
+
+    def create_model(self):
+        n = 1000000
+        raw_data = self.get_ratings()[:n].fillna(0)[["userId", "id", "rating"]]
+        reader = Reader()
+        data = Dataset.load_from_df(raw_data, reader)
+        data.split(n_folds=5)
+        svd = SVD()
+        trainset = data.build_full_trainset()
+        svd.fit(trainset)
+        filename = "C:/datasets/the-movies-dataset/models/collaborative_based/coll_svd.sav"
+        joblib.dump(svd, filename)
+
+    def predict_rating_by_user_movie(self, userid, id):
+        svd = joblib.load("C:/datasets/the-movies-dataset/models/collaborative_based/coll_svd.sav")
+        return svd.predict(userid, id).est
 
 if __name__ == "__main__":
     sys.stdout.buffer.write(chr(9986).encode('utf8'))
     pd.set_option('display.max_columns', 500)
     pd.set_option('display.width', 1000)
-    #SVD_Rec = SVD_Recommender("user")
-    #SVD_Rec.create_model()
+    SVD_Rec = SVDPP_Recommender("user")
+    SVD_Rec.create_model()
     #from surprise.model_selection import train_test_split
 
-
+    sys.exit()
     # Load the movielens-100k dataset (download it if needed),
     #data = Dataset.load_builtin('ml-100k')
     #trainset, testset = train_test_split(data, test_size=.25)
